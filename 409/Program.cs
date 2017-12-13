d8ab8382626d65eac313e93c717264c55dbcca52
Vass9811 <vasilena_9811@abv.bg>
2017-12-13 10:19:02 +0200

    Added some stuff

---
+++ 409/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _409
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведи n:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Стойности на А");
+            int[] A = new int[n];
+            FillArray(A);
+            Console.WriteLine("Стойности на B");
+            int[] B = new int[n];
+            FillArray(B);
+            Console.WriteLine("Стойности на C");
+            int[] C = new int[n];
+            FillArray(C);
+            Console.WriteLine("Средноаритметично на елементите на масива A: {0}",+Average(A));
+            Console.WriteLine("Средноаритметично на елементите на масива B: {0}", +Average(B));
+            Console.WriteLine("Средноаритметично на елементите на масива C:{0}", +Average(C));
+            double[] min = new double[] { Average(A), Average(B), Average(C) };
+            double minaverage = min.Min();
+            Console.WriteLine("Mинималната от трите средноаритм.стойности: {0}",+minaverage);
+
+        }
+        static void FillArray(int[]array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        static double Average(int[]array)
+        {
+            double average = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(array[i]/7==0)
+                {
+                    average += array[i];
+                }
+            }
+            return average / array.Length;
+        }
+        
+    }
+}
---
+++ FirstTask/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert n:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insert hours in A:");
+            int[] A = new int[n];
+            Input(A);
+            Console.WriteLine("Insert hours in B:");
+            int[] B = new int[n];
+            Input(B);
+            Console.WriteLine("Insert hours in C:");
+            int[] C = new int[n];
+            Input(C);
+            Console.WriteLine("Average for A:{0}",+Average(A));
+            Console.WriteLine("Average for B:{0}",+Average(B));
+            Console.WriteLine("Average for C:{0}",+Average(C));
+            double averageA = Average(A);
+            double averageB = Average(B);
+            double averageC = Average(C);
+            Console.WriteLine("Above average for A:");
+            AboveAverage(A, averageA);
+            Console.WriteLine("Above average for B:");
+            AboveAverage(B, averageB);
+            Console.WriteLine("Above average for C:");
+            AboveAverage(C, averageC);
+
+        }
+        static void Input(int[]hours)
+        {
+            for (int i = 0; i < hours.Length; i++)
+            {
+                Console.WriteLine("Day {0}",i+1);
+                hours[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        static double Average(int[]hours)
+        {
+            double average = 0;
+            for (int i = 0; i < hours.Length; i++)
+            {
+                average += hours[i];
+            }
+            return average / hours.Length;
+        }
+        static void AboveAverage(int[]hours,double avrg)
+        {
+            for (int i = 0; i < hours.Length; i++)
+            {
+                if(hours[i]>avrg)
+                {
+                    Console.Write("Day {0}; ", i + 1);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
